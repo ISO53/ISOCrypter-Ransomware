d8320f1af10e3bc541ecebd91cd4d8163d1697da
ISO53 <102249575+ISO53@users.noreply.github.com>
2023-12-04 16:10:51 +0300

    Java class created for handling key generation and encryption

---
+++ EncryptionManager.java
@@ -0,0 +1,39 @@
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+
+public abstract class EncryptionManager {
+
+    public static final SecretKey KEY = generateSecretKey();
+
+    public static byte[] encrypt(byte[] content, SecretKey key) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            return cipher.doFinal(content);
+        } catch (IllegalBlockSizeException | BadPaddingException | InvalidKeyException | NoSuchAlgorithmException
+                | NoSuchPaddingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static SecretKey generateSecretKey() {
+        KeyGenerator keygen;
+        try {
+            keygen = KeyGenerator.getInstance("AES");
+            keygen.init(256, new SecureRandom());
+            return keygen.generateKey();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
