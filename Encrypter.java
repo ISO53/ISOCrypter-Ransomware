58f320dfa90809e4ff26661c88f9de8edaf95f60
ISO53 <102249575+ISO53@users.noreply.github.com>
2023-12-04 16:10:25 +0300

    File encryption method added

--- Encrypter.java
+++ Encrypter.java
@@ -1,6 +1,9 @@
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 
-public class Encrypter implements Runnable{
+public class Encrypter implements Runnable {
 
     Path path;
     Thread thread;
@@ -12,6 +15,31 @@ public class Encrypter implements Runnable{
 
     @Override
     public void run() {
+        // Read file content
+        byte[] fileContent;
+        try {
+            fileContent = Files.readAllBytes(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
 
+        // Append the filename as a new line to EOF so we can find the file name and
+        // extension after decryption
+        String fileNameLine = path.getFileName().toString() + System.lineSeparator();
+        byte[] fileNameLineBytes = fileNameLine.getBytes();
+        byte[] contentWithFileName = Arrays.copyOf(fileContent, fileContent.length + fileNameLineBytes.length);
+        System.arraycopy(fileNameLineBytes, 0, contentWithFileName, fileContent.length, fileNameLineBytes.length);
+
+        // Encrypt the file content
+        byte[] encryptedContent = EncryptionManager.encrypt(contentWithFileName, EncryptionManager.KEY);
+
+        // Save the encrypted content back to file
+        try {
+            Files.write(path, encryptedContent);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
     }
 }
